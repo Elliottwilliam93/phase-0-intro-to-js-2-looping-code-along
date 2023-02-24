217d0eb13cb643a869e9934e2251deecd74e1581
williamelliott <ironmaiden229@hotmail.com>
2023-02-23 21:26:59 -0500

    project finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(cards) {
+    const m = []
+    for (let i = 0; i < cards.length; i++){
+        m.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return m
+}
+
+function countDown(number){
+    for (let i = 0; i <= number; i++){
+    console.log(number-i)
+    }
+}
